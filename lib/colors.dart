d49ad60824b036516fd0a832c27529d54650b7a7
ntua-el21047 <el21047@mail.ntua.gr>
2024-12-14 13:37:42 +0200

    Added assets (frames and icons), updated pubspec.yaml, and added styles

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFFE6E6E6);
+  static const Color frameColor = Color(0xFFD7D7D7);
+  static const Color filtersColor = Color(0xFFF0CFCF);
+  static const Color textColor = Color(0xFFD0B7D4);
+  static const Color titlesColor = Color(0xFFE7BDBD);
+}
---
+++ lib/text_styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  // Title 1 Font
+  static TextStyle title1 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.bold,
+    fontSize: 48,
+  );
+
+  // Title 2 Font
+  static TextStyle title2 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w600,
+    fontSize: 32,
+  );
+
+  // Body 1 Font
+  static TextStyle body1 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w500,
+    fontSize: 24,
+  );
+
+  // Body 2 Font
+  static TextStyle body2 = GoogleFonts.montserrat(
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+  );
+}
